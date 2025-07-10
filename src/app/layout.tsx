c70e7626dc8798dd43e26bc91c736e4a1d0aa6e0
Cameron Campion <cameroncampion2018@gmail.com>
2025-07-10 15:06:46 +0000

    We wanted the Zen Precision UI

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -28,7 +28,7 @@ export default function RootLayout({
 }>) {
   
   return (
-    <html lang="en" className="dark">
+    <html lang="en">
       <head>
         <link rel="preconnect" href="https://fonts.googleapis.com" />
         <link rel="preconnect" href="https://fonts.gstatic.com" crossOrigin="anonymous" />
